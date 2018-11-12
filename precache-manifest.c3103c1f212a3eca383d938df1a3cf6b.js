9e7f0449e9f777507269a3c3a976cc75f028a7bc
vantezzen <properly@protonmail.com>
2018-11-12 20:52:49 +0100

    Add GitHub Pages build

---
+++ precache-manifest.c3103c1f212a3eca383d938df1a3cf6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ebad8e7a9f5566e1598",
+    "url": "/todo-react/static/js/runtime~main.4ebad8e7.js"
+  },
+  {
+    "revision": "63e3cb4e27a2dc54c855",
+    "url": "/todo-react/static/js/main.63e3cb4e.chunk.js"
+  },
+  {
+    "revision": "62d8ccf9ea270606c305",
+    "url": "/todo-react/static/js/1.62d8ccf9.chunk.js"
+  },
+  {
+    "revision": "63e3cb4e27a2dc54c855",
+    "url": "/todo-react/static/css/main.6b1e0d84.chunk.css"
+  },
+  {
+    "revision": "af8b9813b613ad91fe572d32d6f755df",
+    "url": "/todo-react/index.html"
+  }
+];
